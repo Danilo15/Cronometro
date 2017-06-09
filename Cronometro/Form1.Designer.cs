a4fd4775b8cca10bc113e96c9907a8a69515815f
Danilo15 <danilooliveira9@hotmail.com>
2017-06-08 23:24:44 -0300

    initial commit

---
+++ Cronometro/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Cronometro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblCronometro = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(174, 198);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblCronometro
+            // 
+            this.lblCronometro.AutoSize = true;
+            this.lblCronometro.Location = new System.Drawing.Point(187, 153);
+            this.lblCronometro.Name = "lblCronometro";
+            this.lblCronometro.Size = new System.Drawing.Size(64, 17);
+            this.lblCronometro.TabIndex = 1;
+            this.lblCronometro.Text = "00:00:00";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Total:";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(64, 13);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(0, 17);
+            this.lblTotal.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(438, 335);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblCronometro);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblCronometro;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblTotal;
+    }
+}
+
---
+++ Cronometro/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Cronometro
+{
+    public partial class Form1 : Form
+    {
+        private int seconds = 0;
+        private int minutes = 0;
+        private int hours = 0;
+        private int qtdCronometrada = 0;
+        private bool descanso = true;
+        private int tempo = 25;
+        private int tempoPadrao = 25;
+        private int tempoExtendido = 30;
+        private int tempoDescanso = 5;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "Stop!")
+            {
+                timer1.Stop();
+                button1.Text = "Start";
+                SomarCronometrada();
+                tempo = tempoPadrao;
+                return;
+            }
+
+            button1.Enabled = false;
+            Start(tempo);
+        }
+
+        private void Start(int startMinutes)
+        {
+            if(tempo == tempoExtendido)
+            {
+                BotaoParar();
+            }
+
+            timer1 = new Timer();
+            timer1.Interval = 1000;
+
+            timer1.Tick += OnTick;
+
+            minutes = startMinutes;
+            ApplyOnLabel();
+
+            timer1.Start();
+        }
+
+
+        private void SomarCronometrada()
+        {
+            qtdCronometrada++;
+            lblTotal.Text = qtdCronometrada.ToString();
+        }
+        private void OnLimit()
+        {
+            Stop();
+
+            bool isDescanso = descanso && tempo != tempoExtendido;
+
+            if (isDescanso)
+                MessageBox.Show("Descanso");
+
+            if (isDescanso)
+            {
+                Start(tempoDescanso);
+            }
+            else
+            {
+                SomarCronometrada();
+                button1.Enabled = true;
+            }
+
+            if (tempo == tempoExtendido)
+            {
+                tempo = tempoPadrao;
+                button1.Text = "Start";
+            }
+
+            if (qtdCronometrada % 4 == 0)
+            {
+                tempo = tempoExtendido;
+            }
+
+            descanso = !descanso;
+        }
+
+        private void BotaoParar()
+        {
+            button1.Enabled = true;
+            button1.Text = "Stop!";
+        }
+
+        private void Stop()
+        {
+            timer1.Stop();
+        }
+
+        private void ApplyOnLabel()
+        {
+            lblCronometro.Text = string.Format("{0}:{1}:{2}", hours.ToString("00"), minutes.ToString("00"), seconds.ToString("00"));
+        }
+
+        private void OnTick(object sender, EventArgs e)
+        {
+            Timer timer = (Timer)sender;
+
+            Timer send = (Timer)sender;
+
+            if (seconds == 0)
+            {
+                minutes--;
+                seconds = 59;
+            }
+            else
+            {
+                seconds--;
+            }
+
+            ApplyOnLabel();
+
+            if (minutes == 0 && seconds == 0)
+            {
+                OnLimit();
+            }
+        }
+    }
+}
